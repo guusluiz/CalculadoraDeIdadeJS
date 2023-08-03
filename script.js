98bb581ad5f8397ccf815ec0dcae4ede9bf960f6
Gustavo <gustavoluizmiranda405@gmail.com>
2023-08-03 19:30:21 -0300

    calculadora quase finalizada

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function calcularIdade(event) {
-    event.preventDefault()
+function calcularIdade() {
     cadastrarUsuário();
 
 }
@@ -11,29 +10,82 @@ function cadastrarUsuário() {
     let anoRecebido = parseInt(document.getElementById("ano-nascimento").value);
 
     anoAtual = new Date().getFullYear();
+    let idade = anoAtual - anoRecebido;
+    let classificacao;
     let dadosUsuario = {
         nome: nomeRecebido,
         dia: diaRecebido,
         mes: mesRecebido,
         ano: anoRecebido,
         idade: idade,
-        definicao: definicao
+        classificacao: classificacao
+        //precisa de data hora?
     }
 
-    let idade = anoAtual - anoRecebido;
-    console.log(idade);
-    idade = 11
     if (idade <= 12) {
-        console.log("Criança");
+        classificacao = "Criança";
 
     } else if (idade < 18 && idade >= 13) {
-        console.log("Adolescente");
+        classificacao = "Adolescente";
 
     } else if (idade < 65 && idade >= 18) {
-        console.log("Adulto");
+        classificacao = "Adulto";
 
     } else if (idade >= 65) {
-        console.log("Idoso");
+        classificacao = "Idoso";
     }
+
+    let listaUsuarios = [];
+
+    if (localStorage.getItem("usuariosCadastrados")) {
+        listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"))
+    }
+    listaUsuarios.push(dadosUsuario);
+
+    localStorage.setItem("usuariosCadastrados", JSON.stringify(listaUsuarios))
+}
+
+function carregarUsuario() {
+    let listaUsuarios = []
+
+    if (localStorage.getItem("usuariosCadastrados")) {
+        listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"))
+    }
+    if (listaUsuarios.length == 0) {
+        let tabela = document.getElementById("corpo_tabela");
+
+        tabela.innerHTML = `<tr class="linha-mensagem">
+        <td colspan="4">Nenhum usuário cadastrado!</td>
+    </tr>`
+    } else {
+        MontarTabela(listaUsuarios);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => carregarUsuario());
+
+function MontarTabela(listaDeCadastrados) {
+    let tabela = document.getElementById("corpo-tabela");
+
+    let template = '';
+
+    listaDeCadastrados.forEach(pessoa => {
+        template += `<tr>
+            <td data-cell="nome">${pessoa.nome}</td>
+            <td data-cell="data de nascimento">${pessoa.dia}/${pessoa.mes}/${pessoa.ano}</td>
+            <td data-cell="idade">${pessoa.idade}</td>
+            <td data-cell="Faixa Etária">${pessoa.classificacao}</td>
+        </tr>`
+
+        tabela.innerHTML = template;
+    })
+}
+
+
+//Passo 8 - Limpar local storage
+function deletarRegistro() {
+    localStorage.clear()
+    window.location.reload();
+
 
 }
